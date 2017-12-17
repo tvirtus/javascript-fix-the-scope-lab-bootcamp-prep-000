4157713f74e47119a79e34b7267fbdddc6fb606f
Victor Trofimciuc <victortrofimciuc@gmail.com>
2017-12-17 17:57:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  var two = 2;
-  return n + two
+  const two = 2;
+  return n + two;
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
