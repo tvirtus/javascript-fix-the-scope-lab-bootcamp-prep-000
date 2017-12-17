feec73cf1bb0389dbe32aefe821a96051a305d1f
Victor Trofimciuc <victortrofimciuc@gmail.com>
2017-12-17 17:58:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
+  return (function() {
     return "FUNKY!"
-  }();
+  })();
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
